c5d78dfb742ce6912b61171e5639ea370499b382
saint299999@gmail.com <saint299999@gmail.com>
2024-05-06 22:58:16 +0300

    Руты для статей

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,18 @@ Router::get('/auth/register', 'app\Controllers\AuthController@showRegister');
 Router::post('/auth/register', 'app\Controllers\AuthController@register');
 
 Router::get('/auth/logout', 'app\Controllers\AuthController@logout');
+
+
+Router::get('/articles', 'app\Controllers\ArticlesController@index');
+
+Router::get('/articles/store', 'app\Controllers\ArticlesController@create');
+
+Router::post('/articles/store', 'app\Controllers\ArticlesController@store');
+
+Router::get('/articles/show', 'app\Controllers\ArticlesController@show');
+
+Router::get('/articles/update', 'app\Controllers\ArticlesController@edit');
+
+Router::patch('/articles/update', 'app\Controllers\ArticlesController@update');
+
+Router::delete('/articles/destroy', 'app\Controllers\ArticlesController@destroy');
