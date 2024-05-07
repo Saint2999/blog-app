149589b0b2df06409c9da3fb7fb04f423e5f1160
saint299999@gmail.com <saint299999@gmail.com>
2024-05-08 01:01:56 +0300

    Руты для лайков

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,26 @@ Router::get('/', 'app\Controllers\ArticlesController@index');
 Router::get('/articles', 'app\Controllers\ArticlesController@index');
 
 Router::get('/articles/store', 'app\Controllers\ArticlesController@create');
-
 Router::post('/articles/store', 'app\Controllers\ArticlesController@store');
 
 Router::get('/articles/show', 'app\Controllers\ArticlesController@show');
 
 Router::get('/articles/update', 'app\Controllers\ArticlesController@edit');
-
 Router::post('/articles/update', 'app\Controllers\ArticlesController@update');
 
 Router::post('/articles/destroy', 'app\Controllers\ArticlesController@destroy');
 
 
 Router::get('/comments/store', 'app\Controllers\CommentsController@create');
-
 Router::post('/comments/store', 'app\Controllers\CommentsController@store');
 
 Router::get('/comments/update', 'app\Controllers\CommentsController@edit');
-
 Router::post('/comments/update', 'app\Controllers\CommentsController@update');
 
 Router::post('/comments/destroy', 'app\Controllers\CommentsController@destroy');
+
+
+
+Router::post('/likes/store', 'app\Controllers\LikesController@store');
+
+Router::post('/likes/destroy', 'app\Controllers\LikesController@destroy');
