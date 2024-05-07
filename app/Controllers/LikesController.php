8e6cea3af7be2bd6b52f0fe6ffada39cc74ce7db
saint299999@gmail.com <saint299999@gmail.com>
2024-05-08 01:02:16 +0300

    Контроллер для лайков

---
+++ app/Controllers/LikesController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\Controllers;
+
+use app\Core\Request;
+use app\Core\Response;
+use app\Core\SessionManager;
+use app\Services\LikesService;
+use app\DTOs\LikeDTO;
+use app\Helpers\DTOHydrator;
+use app\Helpers\Redirector;
+
+class LikesController
+{
+    private LikesService $service;
+
+    public function __construct()
+    {
+        $this->service = new LikesService();
+    }
+
+    public function store(Request $request): ?Response
+    {
+        if (!SessionManager::has('authenticated')) {
+            Redirector::redirect('/auth/login');
+        }
+
+        $articleId = $request->getParam('article_id');
+
+        $token = $request->getParam('csrf-token');
+
+        if (!$token || !hash_equals(SessionManager::get('csrf-token'), $token)) {
+            SessionManager::set('csrf-token', bin2hex(random_bytes(32)));
+
+            Redirector::redirect("/articles/show?id=$articleId");
+        }
+
+        $likeDTO = DTOHydrator::hydrate(
+            $request->getParams(),
+            new LikeDTO()
+        );
+
+        $this->service->storeLike($likeDTO);
+
+        Redirector::redirect("/articles/show?id=$articleId"); 
+    }
+
+    public function destroy(Request $request): ?Response
+    {
+        if (!SessionManager::has('authenticated')) {
+            Redirector::redirect('/auth/login');
+        }
+
+        $articleId = $request->getParam('article_id');
+
+        $token = $request->getParam('csrf-token');
+
+        if (!$token || !hash_equals(SessionManager::get('csrf-token'), $token)) {
+            SessionManager::set('csrf-token', bin2hex(random_bytes(32)));
+
+            Redirector::redirect("/articles/show?id=$articleId");
+        }
+        
+        $this->service->destroyLikeByArticleId($articleId);
+        
+        Redirector::redirect("/articles/show?id=$articleId");
+    }
+}
