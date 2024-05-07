346dde3f4521e18523683dd08735926c0dfabf0c
saint299999@gmail.com <saint299999@gmail.com>
2024-05-07 22:44:01 +0300

    Репозиторий для комментариев

---
+++ app/Repositories/CommentsRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\Repositories;
+
+use app\Models\Comment;
+
+class CommentsRepository 
+{
+    public function getCommentsByArticleId(string $id): array
+    {
+        return Comment::whereAndOrderBy('article_id', $id, 'created_at', 'ASC');
+    }
+
+    public function getCommentById(string $id): ?Comment
+    {
+        return Comment::find($id);
+    }
+
+    public function storeComment(array $details): ?Comment
+    {
+        return Comment::create($details);
+    }
+
+    public function updateComment(array $details, string $id): ?Comment
+    {
+        return Comment::update($details, $id);
+    }
+
+    public function destroyCommentById(string $id): bool
+    {
+        return Comment::delete($id);
+    }
+}
