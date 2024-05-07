fd1dfdada174d867d26381ba1f479fcd2ba03bd4
saint299999@gmail.com <saint299999@gmail.com>
2024-05-08 01:02:45 +0300

    Репозиторий для лайков

---
+++ app/Repositories/LikesRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\Repositories;
+
+use app\Models\Like;
+
+class LikesRepository 
+{
+    public function getLikeCountByArticleId(string $id): int 
+    {
+        return count(Like::where('article_id', $id));
+    }
+
+    public function storeLike(array $details): bool
+    {
+        $like = Like::create($details);
+
+        if (!$like) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function destroyLike(string $articleId, string $userId): bool
+    {
+        $likes = Like::whereAndWhere('article_id', $articleId, 'user_id', $userId);
+
+        $like = reset($likes);
+
+        if (!$like) {
+            return false;
+        }
+
+        return Like::delete($like->id);
+    }
+}
